d35e1fbd92811cc45d53fe4322318b9c5036a578
Christian Butcher <chrisb2244@gmail.com>
2018-11-12 17:18:55 +0900

    Add .mk files for the AF_Messages-PPL and Actor Framework libraries. These correctly identify dependencies but do not currently build the PPLs.

---
+++ AF_Messages-PPL/AF_Messages-PPL.mk
@@ -0,0 +1,8 @@
+#AF_Messages-PPL mk file
+AF_Messages-PPL.lvlibp : $(call ADD_SOURCES,AF_Messages-PPL.mk) \
+		Actor\ Framework.lvlibp \
+		| $(PPLDIR)
+	$(info Target is $@)
+#	$(info Dependencies are $^)
+	$(info 'Making' $@ from source)
+	$(TOUCH) $(PPLDIR)/$@
---
+++ ActorFramework/Actor Framework.mk
@@ -0,0 +1,12 @@
+#Actor Framework mk file
+$(call pS,Actor+Framework.lvlibp) : $(call ADD_SOURCES,Actor\ Framework.mk) \
+		| $(PPLDIR)
+#	$(info Makefile being run is at $(call GET_CURRENT_MKFILE,Actor\ Framework.mk))
+#	$(info Path found is $(call GET_MKFILE_DIR,Actor\ Framework.mk))
+	$(info Target is $@)
+#	$(info Dependencies are $^)
+	$(info 'Making' $@ from source)
+# Here, we can't use $@ because it doesn't produce the correct filename... :(
+# Either literal typing (here) or the pS function (in target) works for this line.
+	$(TOUCH) $(PPLDIR)/Actor\ Framework.lvlibp
+
