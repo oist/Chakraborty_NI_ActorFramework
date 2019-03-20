b0bc695c09ca1095547c6757ccfcb2665d5df795
Christian Butcher <chrisb2244@gmail.com>
2019-03-20 20:15:57 +0900

    Update mkfiles.

--- AF_Messages-PPL/AF_Messages-PPL.mk
+++ AF_Messages-PPL/AF_Messages-PPL.mk
@@ -1,6 +1,2 @@
-#AF_Messages-PPL mk file
-AF_Messages-PPL.lvlibp : $(call ADD_SOURCES,AF_Messages-PPL.mk) \
-		Actor\ Framework.lvlibp \
-		| $(PPLDIR)
-	$(TARGET_INFO)
-	$(BUILD_PPL)
+# Define the list of direct dependencies
+AF_Messages-PPL_Deps := Actor\ Framework.lvlibp
--- ActorFramework/Actor Framework.mk
+++
@@ -1,4 +0,0 @@
-Actor\ Framework.lvlibp : $(call ADD_SOURCES,Actor Framework.mk) \
-		| $(PPLDIR)
-	$(TARGET_INFO)
-	$(BUILD_PPL)
